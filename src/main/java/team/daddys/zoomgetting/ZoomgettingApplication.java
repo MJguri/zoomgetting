cf19966673cda94203578385bddc833001e1154b
MJguri <pmy20804@gmail.com>
2022-02-15 22:03:37 +0900

    first commit

---
+++ src/main/java/team/daddys/zoomgetting/ZoomgettingApplication.java
@@ -0,0 +1,13 @@
+package team.daddys.zoomgetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoomgettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoomgettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team/daddys/zoomgetting/ZoomgettingApplicationTests.java
@@ -0,0 +1,13 @@
+package team.daddys.zoomgetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoomgettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
