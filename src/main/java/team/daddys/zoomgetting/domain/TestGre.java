caebd9deb5db7021422729cec97b38c7e46467a0
MJguri <pmy20804@gmail.com>
2022-02-24 17:58:43 +0900

    add testGre

---
+++ src/main/java/team/daddys/zoomgetting/domain/TestGre.java
@@ -0,0 +1,11 @@
+package team.daddys.zoomgetting.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TestGre {
+    private String id;
+    private String password;
+}
